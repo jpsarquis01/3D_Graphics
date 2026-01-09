e59f18d94401a865f557372da3b1c14e3e2fbd45
jpsarquis01 <sarquisjuanpablo@gmail.com>
2026-01-08 21:45:45 -0800

    Class1 stuff

--- Pix/CmdSetColor.h
+++ Pix/CmdSetColor.h
@@ -1,22 +1,20 @@
 #pragma once
-
 #include "Command.h"
 
-class CmdSetColor : public Command
+class CmdSetColor :public Command
 {
 public:
 	const char* GetName() override
 	{
 		return "SetColor";
 	}
-
 	const char* GetDescription() override
 	{
 		return
 			"SetColor(r, g, b)\n"
 			"\n"
-			"- Sets the color using red, green, and blue. Values are from 0.0 to 1.0.";
+			"- Sets the color of the  next pixel using red, green and blue\n"
+			"- Values are from 0,0 - 1.0";
 	}
-
 	bool Execute(const std::vector<std::string>& params) override;
 };
